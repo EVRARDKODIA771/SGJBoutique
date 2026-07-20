4ce20d30760cc3d8fc7c9a67b23968437f4b48df
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-07-20 16:20:10 +0000

    Create supabaseAdmin.js

---
+++ apps/backend/src/lib/supabaseAdmin.js
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+import { env } from "../config/env.js";
+
+export const supabaseAdmin = createClient(
+  env.SUPABASE_URL,
+  env.SUPABASE_SECRET_KEY,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+      detectSessionInUrl: false,
+    },
+  }
+);
